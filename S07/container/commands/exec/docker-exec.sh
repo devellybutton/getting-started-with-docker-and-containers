0e74f4861cdfc154cb10daed0a975a397041cbd6
devellybutton <leegr0106@gmail.com>
2025-03-18 11:12:41 +0900

    S7 docker exec: 컨테이너 내부에 프로세스 실행

--- S07/container/commands/exec/docker-exec.sh
+++ S07/container/commands/exec/docker-exec.sh
@@ -1,13 +1,18 @@
 # 무한히 sleep 상태
 docker run -d --name busybox busybox sleep infinite
 
+# 실행 중인 컨테이너에 접속 (대화형 터미널)
 docker exec -it busybox sh
 
+# 컨테이너 내부에서 프로세스 목록 확인
 # 실제로 sh 프로세스가 하나 추가되어 있음
 ps
 
+# 컨테이너 내부 셸 종료
 exit
 
+# 외부에서 컨테이너 프로세스 확인
 docker exec busybox ps
 
+# 컨테이너 강제 삭제
 docker rm -f busybox
